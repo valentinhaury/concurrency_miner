b39105ec48f0eaeff4b6d79bbc623c50d0191422
unknown <valentinhaury@gmail.com>
2026-06-18 11:19:17 +0200

    added getter for relations to log

--- src/data_structures/log.py
+++ src/data_structures/log.py
@@ -1,19 +1,36 @@
 class Log:
     def __init__(self, traces):
-        self.log = traces
+        self.traces = traces
 
     def __str__(self):
         string = "("
-        for trace in self.log:
+        for trace in self.traces:
             string += str(trace) + ",    "
         string = string[:-5] + ")"
         return string
 
     def add_trace(self, trace):
-        self.log.append(trace)
+        self.traces.append(trace)
 
     def get_traces(self):
-        return self.log
+        return self.traces
 
-    def number_of_traces(self):
-        return len(self.log)
+    def get_eventually_follows_relations_by_label(self):
+        eventually_follows_relations = []
+        for trace in self.traces:
+            trace_relations = trace.get_eventually_follows_relations()
+            for relation in trace_relations:
+                if not relation.exists_by_label(eventually_follows_relations):
+                    eventually_follows_relations.append(relation)
+
+        return eventually_follows_relations
+
+    def get_overlapping_relations_by_label(self):
+        overlapping_relations = []
+        for trace in self.traces:
+            trace_relations = trace.get_overlapping_relations()
+            for relation in trace_relations:
+                if not relation.exists_by_label(overlapping_relations):
+                    overlapping_relations.append(relation)
+
+        return overlapping_relations
