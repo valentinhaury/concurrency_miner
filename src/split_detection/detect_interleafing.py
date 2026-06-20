dfb1a381ffee7f1cd1100cd95c88fe3440e4275e
unknown <valentinhaury@gmail.com>
2026-06-20 10:55:40 +0200

    exportet functions from detect_interleafing to detection_helper_functions

--- src/split_detection/detect_interleafing.py
+++ src/split_detection/detect_interleafing.py
@@ -1,25 +1,8 @@
-from src.data_structures.directly_follows_relation import DirectlyFollowsRelation
 from src.data_structures.overlapping_relation import OverlappingRelation
-
+from src.split_detection.detection_helper_functions import are_in_loop, fully_connected
 
 def detect_interleafing(log):
     return len(create_interleafing_partitions(log)) > 1
-
-def fully_connected(a1, a2, directly_follows_relations):
-    if (not DirectlyFollowsRelation(a1, a2).relation_exists_by_label(directly_follows_relations)
-     or not DirectlyFollowsRelation(a2, a1).relation_exists_by_label(directly_follows_relations)
-     ):
-        return False
-    return True
-
-def are_in_loop(a1, a2, log):
-    start = log.get_start_activities_by_label()
-    end = log.get_end_activities_by_label()
-    if not a1.activity_exists_by_label(start) and not a1.activity_exists_by_label(end):
-        return True
-    if not a2.activity_exists_by_label(start) and not a2.activity_exists_by_label(end):
-        return True
-    return False
 
 def create_interleafing_partitions(log):
     directly_follows_relations = log.get_directly_follows_relations_by_label()
--- src/split_detection/detection_helper_functions.py
+++ src/split_detection/detection_helper_functions.py
@@ -1,4 +1,4 @@
-from src.data_structures.activity import Activity
+from src.data_structures.directly_follows_relation import DirectlyFollowsRelation
 
 def are_in_loop(a1, a2, log):
     start = log.get_start_activities_by_label()
@@ -8,3 +8,10 @@ def are_in_loop(a1, a2, log):
     if not a2.activity_exists_by_label(start) and not a2.activity_exists_by_label(end):
         return True
     return False
+
+def fully_connected(a1, a2, directly_follows_relations):
+    if (not DirectlyFollowsRelation(a1, a2).relation_exists_by_label(directly_follows_relations)
+     or not DirectlyFollowsRelation(a2, a1).relation_exists_by_label(directly_follows_relations)
+     ):
+        return False
+    return True
