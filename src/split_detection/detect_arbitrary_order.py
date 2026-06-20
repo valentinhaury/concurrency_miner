41195657415433cd5caa609d5c176273bab48433
unknown <valentinhaury@gmail.com>
2026-06-20 11:34:36 +0200

    started arbitrary order partitioning

--- src/split_detection/detect_arbitrary_order.py
+++ src/split_detection/detect_arbitrary_order.py
@@ -1,2 +1,23 @@
+from data_structures import eventually_follows_relation
+from data_structures.log import Log
+
+
 def detect_arbitrary_order(log):
     return False
+
+def create_arbitrary_order_partitions(log):
+    eventually_follows_relations = log.get_eventually_follows_relations_by_label()
+    overlapping_relation =log.get_overlapping_relations_by_label()
+    partitions = []
+    activities = log.get_activities_by_label()
+
+    while activities:
+        new_partition = [activities.pop()]
+        partitions.append(new_partition)
+
+        changed = True
+        while changed:
+            changed = False
+
+            if not activities:
+                break
--- src/split_detection/detect_interleafing.py
+++ src/split_detection/detect_interleafing.py
@@ -10,22 +10,23 @@ def create_interleafing_partitions(log):
     activities = log.get_activities_by_label()
     partitions = []
 
-    while activities:
+    while activities:                                           #WHILE LOOP to create new partitions
         new_partition = [activities.pop()]
         partitions.append(new_partition)
 
         changed = True
-        while changed:
+        while changed:                                          #WHILE LOOP to update partitions by adding activities until nothing changes
             changed = False
             if not activities:
                 break
-            for partition in partitions:
+                                                                #TODO Maybe i just need to run the following for the new_partition and not for all partitions in partitions
+            for partition in partitions:                        #check all existing partitions
                 if not activities:
                     break
-                for a1 in partition:
+                for a1 in partition:                            #for all activities in the current partition check:
                     activities_save = []
                     length_activities = len(activities)
-                    for i in range(length_activities):
+                    for i in range(length_activities):          #for all activities that are in no partition check to see if they should be added
                         a2 = activities.pop()
                         if OverlappingRelation(a1, a2).relation_exists_by_label(overlapping_relations) or OverlappingRelation(a2, a1).relation_exists_by_label(overlapping_relations):
                             if a2 not in partition:
