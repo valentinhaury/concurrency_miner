085e320b4effecde9cf6eac9b012b1ae5147c5db
unknown <valentinhaury@gmail.com>
2026-07-14 10:14:04 +0200

    added comments

--- src/concurrency_miner.py
+++ src/concurrency_miner.py
@@ -14,9 +14,12 @@ from src.split_detection.detect_multi_instance import get_multi_instance_activit
 
 
 def concurrency_miner(log, multi_instance_activities=None):
+# check for multi_instance activities
     if not multi_instance_activities:
         multi_instance_activities = get_multi_instance_activities(log)
+# add Activity("tau") to empty traces
     handle_empty_traces(log)
+# end recursion and add single activity node
     if detect_single_activity(log):
         activity = get_single_activity(log)
         if activity.activity_exists_by_label(multi_instance_activities):
@@ -25,36 +28,43 @@ def concurrency_miner(log, multi_instance_activities=None):
         else:
             process_tree = Node(activity)
         return process_tree
+# split the log with an exclusive choice operator
     elif detect_exclusive(log):
         process_tree = Node(Operator.Exclusive)
         for sublog in get_exclusive_choice_sublogs(log):
             process_tree.add_child(concurrency_miner(sublog, multi_instance_activities))
         return process_tree
+# split the log with a sequence operator
     elif detect_sequence(log):
         process_tree = Node(Operator.Sequence)
         for sublog in get_sequence_sublogs(log):
             process_tree.add_child(concurrency_miner(sublog, multi_instance_activities))
         return process_tree
+# split the log with an arbitrary order operator
     elif detect_arbitrary_order(log):
         process_tree = Node(Operator.Arbitrary)
         for sublog in get_arbitrary_order_sublogs(log):
             process_tree.add_child(concurrency_miner(sublog, multi_instance_activities))
         return process_tree
+# split the log with an interleaving operator
     elif detect_interleafing(log):
         process_tree = Node(Operator.Interleafing)
         for sublog in get_interleafing_sublogs(log):
             process_tree.add_child(concurrency_miner(sublog, multi_instance_activities))
         return process_tree
+# split the log with a concurrent operator
     elif detect_concurrent(log):
         process_tree = Node(Operator.Concurrent)
         for sublog in get_concurrent_sublogs(log):
             process_tree.add_child(concurrency_miner(sublog, multi_instance_activities))
         return process_tree
+# split the log with a parallel operator
     elif detect_parallel(log):
         process_tree = Node(Operator.Parallel)
         for sublog in get_parallel_sublogs(log):
             process_tree.add_child(concurrency_miner(sublog, multi_instance_activities))
         return process_tree
+# split the log with a loop operator
     elif detect_loop(log):
         process_tree = Node(Operator.Loop)
         for sublog in get_loop_sublogs(log):
